4d760b06ad05ac3ae20b5f6fba0a515d87ff5795
RKNyra <rk.nyra@gmail.com>
2020-02-07 13:12:33 +0300

    rectify sender email

--- intranet/email.py
+++ intranet/email.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 
 def send_credentials(password,username,reciever):
     subject = 'Pawame Password Management.'
-    sender = 'danielevans.karani@gmail.com'
+    sender = 'rknyramoringa@gmail.com'
 
     #passing in the context variables
     text_content = render_to_string('email/passwordemail.txt', {"password":password, "username":username})
